6349005849b0f086affb5090316815a3cb492cb0
MoZhuXuan10 <1214766435@qq.com>
2018-06-03 12:38:39 +0800

    autowire自动装配和spel

---
+++ 04Spring/src/main/java/com/syl/bean/Dog.java
@@ -0,0 +1,33 @@
+package com.syl.bean;
+
+/**
+ * 小狗的实体类
+ */
+public class Dog {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
